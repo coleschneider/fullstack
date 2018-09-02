84ed73c63db0dde2f971bad7043b35a9c03fdb77
Cole Schneider <colekschneider@gmail.com>
2018-09-01 21:46:57 -0700

    added sumall to feature

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ const DjanstraAlgo = (a, b) => {
   return DjanstraAlgo(b, a % b)
 }
 console.log(DjanstraAlgo(10, 5))
+
+const sumAll = (numArr) => {
+  return numArr.reduce((acc, num) => {
+  acc+=num
+  return acc
+})
+}
+
+console.log(sumAll([3, 4, 12]))
